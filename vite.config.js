65501aec9b7a1f52fb6024e7ea7bd64f46aced34
Byajbook Dev <byajbook@gmail.com>
2026-05-11 15:02:25 +0530

    Optimize chunk size by splitting vendor and firebase

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/tiffin-tracker/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) {
+              return 'firebase';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
